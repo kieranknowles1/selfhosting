265b960c80a0636e17582f8bd9e85245788c96ea
Kieran <kieranknowles11@hotmail.co.uk>
2024-01-10 22:22:08 +0000

    Hopefully fix the backup script in cron

--- backup.sh
+++ backup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Run from cron, so need to CD here from home directory
+cd "$(dirname "$0")"
+
 # Backup script for self-hosted runner
 # Pauses containers, runs Borgmatic, and resumes containers
 # WARN: If a backup fails, containers will not be resumed. A server reboot is recommended in this case
 
 # Exit on error
 set -e
+
+BORG_SERVICE="services/borgmatic"
 
 # Load environment variables
 source .env
@@ -17,16 +22,23 @@ source .env.user
 #===============================================================================
 echo "[INFO] Containers going PAUSED for backup"
 for dir in services/*; do
+  # We need the Borgmatic container, so don't pause it
+  if [[ "$dir" == $BORG_SERVICE ]]; then
+    continue
+  fi
+
   echo "[INFO] Pausing $dir"
   docker-compose --file "$dir/docker-compose.yml" pause
 done
 
+echo "[INFO] Containers paused. Starting backup"
 
 #===============================================================================
 ### Backup
 #===============================================================================
-# Resume Borgmatic as we need it for the backup
-docker-compose --file services/borgmatic/docker-compose.yml unpause
+
+# Start Borgmatic if it's not already running
+docker-compose --file services/borgmatic/docker-compose.yml up --detach
 
 # Run Borgmatic
 docker-compose --file services/borgmatic/docker-compose.yml \
@@ -37,6 +49,11 @@ docker-compose --file services/borgmatic/docker-compose.yml \
 #===============================================================================
 echo "[INFO] Containers going UNPAUSED after backup"
 for dir in services/*; do
+  # It was never paused, so don't resume it
+  if [[ "$dir" == $BORG_SERVICE ]]; then
+    continue
+  fi
+
   echo "[INFO] Unpausing $dir"
   docker-compose --file "$dir/docker-compose.yml" unpause
 done
--- renew.sh
+++ renew.sh
@@ -1,3 +1,6 @@
+# Run from cron, so need to CD here from home directory
+cd "$(dirname "$0")"
+
 source .env
 source .env.user
 docker-compose -f ./services/nginx/docker-compose.yml \
