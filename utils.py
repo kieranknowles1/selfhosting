4e2102304c7c0be90e07fe0b8da8321d2a1194aa
Kieran <kieranknowles11@hotmail.co.uk>
2024-02-04 20:50:56 +0000

    Stronger typing

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 from os import geteuid
+from typing import Dict
 import yaml
 
 def is_root() -> bool:
     return geteuid() == 0
 
-def read_yml_env(path: str) -> dict:
+def read_yml_env(path: str) -> Dict[str, str]:
     with open(path, 'r') as file:
         return yaml.safe_load(file)
