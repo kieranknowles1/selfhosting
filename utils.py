ab0fcb124fc4f30a2d917fb8d2c158e16eb835be
Kieran <kieranknowles11@hotmail.co.uk>
2024-02-05 21:48:02 +0000

    Check environment var types

--- utils.py
+++ utils.py
@@ -18,7 +18,8 @@ def is_root() -> bool:
 def read_yml_env(path: str) -> Dict[str, str]:
     """
     Read a yaml file and return it as a dictionary.
-    The env file is expected to be a dictionary of string-string pairs.
+    All keys must be strings.
+    All values must be strings, numbers, or booleans. Non-string values will be converted to strings.
 
     Parameters
     ----------
@@ -29,6 +30,31 @@ def read_yml_env(path: str) -> Dict[str, str]:
     -------
     Dict[str, str]
         The contents of the yaml file as a dictionary.
+
+    Raises
+    ------
+    Exception
+        If the file at `path` is not a valid yaml file or
+        if the file at `path` is not a dictionary of string-string pairs.
     """
+
     with open(path, "r") as file:
-        return yaml.safe_load(file)
+        env = yaml.safe_load(file)
+
+    if not isinstance(env, dict):
+        raise Exception(f"{path} is not a dictionary.")
+    for key, value in env.items():
+        if not isinstance(key, str):
+            raise Exception(f"{path} {key} is not a string.")
+
+        # Convert values to strings.
+        if isinstance(value, str):
+            continue
+        elif isinstance(value, int) or isinstance(value, float):
+            env[key] = str(value)
+        elif isinstance(value, bool):
+            env[key] = "true" if value else "false"
+        else:
+            raise Exception(f"{path} {key} is not a string, number, or boolean.")
+
+    return env
