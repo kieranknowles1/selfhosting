164877dcb29f36f2b3cfe90f840dc53a6dd692ab
Kieran <kieranknowles11@hotmail.co.uk>
2024-02-08 22:02:16 +0000

    Update immich to 1.94.1 This required adding new headers to nginx to allow websockets

--- setup.sh
+++ setup.sh
@@ -83,6 +83,8 @@ generate_nginx_config() {
             proxy_set_header X-Real-IP §remote_addr;
             proxy_set_header X-Forwarded-For §proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto §scheme;
+            proxy_set_header Upgrade §http_upgrade;
+            proxy_set_header Connection \"Upgrade\";
         }
     }
   "
