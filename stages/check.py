44eb232d22bba27b5ba175c6ade8ea3b74f57bcd
Kieran <kieranknowles11@hotmail.co.uk>
2024-04-11 23:42:32 +0100

    List all files failing checks

--- stages/check.py
+++ stages/check.py
@@ -2,11 +2,31 @@
 
 from logging import getLogger
 from jsonschema import validate, ValidationError
+from typing import Any
 import yaml
 
 from utils import service
 
 logger = getLogger(__name__)
+
+def check_schema(schema: Any, data: Any, *, allow_empty: bool = False):
+    """
+    Check that data is valid according to schema.
+
+    Parameters
+    ----------
+    :allow_empty: bool
+        If True, allow data to be empty.
+
+    Raises
+    ------
+    :ValidationError:
+        If data is invalid.
+    """
+    if not data and allow_empty:
+        return # Nothing to validate, and caller says it's okay
+
+    validate(data, schema)
 
 def check_schemas():
     """Check that service.yml files are valid."""
@@ -15,18 +35,29 @@ def check_schemas():
     with open("schemas/service.schema.yml") as file:
         schema = yaml.safe_load(file)
 
+    errors: list[str] = []
+
     for item in service.list():
         spec = service.load_spec(f"services/{item}/service.yml")
 
-        # An empty spec is valid
-        if spec:
-            try:
-                validate(spec, schema)
-            except ValidationError as e:
-                logger.error(f"Service {item} has an invalid service.yml file")
-                raise e
+        try:
+            check_schema(schema, spec, allow_empty=True)
+        except ValidationError:
+            errors.append(f"services/{item}/service.yml does not match schema")
+
+    return errors
 
 def check_all():
     """Run all checks."""
-    check_schemas()
-    logger.info("All checks passed")
+    errors: list[str] = []
+
+    errors.extend(check_schemas())
+
+    # logger.info("All checks passed")
+
+    if len(errors) > 0:
+        for error in errors:
+            logger.error(error)
+        raise Exception("Some checks failed.")
+    else:
+        logger.info("All checks passed")
