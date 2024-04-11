d4ce3757e19e814057d9ca17eef61371ee31ffbe
Kieran <kieranknowles11@hotmail.co.uk>
2024-04-11 23:13:18 +0100

    Configure tilde

---
+++ services/nginx/prepare.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+from os import environ
+from typing import Any
+import json
+import yaml
+
+def generate_commands(specs: dict[str, Any]):
+    commands = []
+    for service, spec in specs.items():
+        if "domains" in spec:
+            for domain in spec["domains"]:
+                # TODO: Shortcut field
+                shortcut = domain["domain"][0:2]
+                commands.append([shortcut, {
+                    "url": f"https://{domain['domain']}.{environ['DOMAIN_NAME']}",
+                    "name": domain["name"],
+                }])
+
+    return commands
+
+def main():
+    with open("/tmp/self-hosted-setup/combined-specs.yml") as file:
+        specs = yaml.safe_load(file)
+
+    commands = generate_commands(specs)
+    with open("commands.generated.json", "w") as file:
+        json.dump(commands, file, indent=2)
+
+if __name__ == "__main__":
+    main()
