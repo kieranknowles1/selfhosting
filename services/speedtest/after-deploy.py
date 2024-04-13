fed23cb61fda0e700aaf968250ec063586baa9a7
Kieran <kieranknowles11@hotmail.co.uk>
2024-04-13 22:40:35 +0100

    Fix wud script

--- services/speedtest/after-deploy.py
+++ services/speedtest/after-deploy.py
@@ -5,19 +5,15 @@ from os import environ
 import sqlite3
 
 def hash_password(password: str) -> str:
-    """Hash a password using PHP's default algorithm. WARN: Input is not sanitized! DO NOT USE ON A PUBLIC API!"""
-
-    with docker.from_env() as client:
-        container = client.containers.run(
-            "php:7.4-cli",
-            f"php -r 'echo password_hash(\"{password}\", PASSWORD_DEFAULT);'",
-            remove=True
-        )
-        return container.decode("utf-8").strip()
+    # TODO: Hash for PHP
+    return password
 
 
 def configure():
     print("Configuring speedtest")
+
+    # TODO: Database is owned as root. Can't currently access it
+    return
 
     db_path = f"{environ['DATA_ROOT']}/speedtest/database.sqlite"
     password_hash = hash_password(environ['ADGUARD_PASSWORD'])
@@ -27,19 +23,19 @@ def configure():
         # Run every 15 minutes
         cursor.execute(
             "UPDATE settings SET payload = ? WHERE name = 'speedtest_schedule'",
-            (environ['SPEEDTEST_SCHEDULE'])
+            (environ['SPEEDTEST_SCHEDULE'],)
         )
 
         # Prune old data
         cursor.execute(
             "UPDATE settings SET payload = ? WHERE name = 'prune_results_older_than'",
-            (environ['SPEEDTEST_RETENTION'])
+            (environ['SPEEDTEST_RETENTION'],)
         )
 
         # Secure the admin account
         cursor.execute(
             "UPDATE users SET email = ?, password = ? WHERE name = 'Admin'",
-            (environ['OWNER_EMAIL'], password_hash)
+            (environ['OWNER_EMAIL'], password_hash,)
         )
         # TODO: The schedule never gets applied. Probably need to manually add the cron job. Have a look at source code
 
